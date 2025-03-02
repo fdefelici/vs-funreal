034abd48334bc44bcd89c79e4f675ea9ba5591f8
Federico De Felici <fdf8210@gmail.com>
2025-03-02 18:13:56 +0100

    #40 update unit test for taking into account Native and Foreign project types

--- FUnrealTest/Sources/Units/FUnrealService_CreateTest.cs
+++ FUnrealTest/Sources/Units/FUnrealService_CreateTest.cs
@@ -1,31 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 using FUnreal;
-using System.Collections.Generic;
-using System.Threading;
-using System.IO;
+using System;
 
 namespace FUnrealTest
 {
     [TestClass]
     public class FUnrealService_CreateTest
     {
-        string tmpPath;
+        string rootPath;
 
         [TestInitialize]
         public void SetUp()
         {
-            string resPath = TestUtils.AbsPath("Resources", "FUnrealService_CreateTest");
-            tmpPath = TestUtils.AbsPath("FUnrealService_CreateTest");
-
-            TestUtils.DeleteDir(tmpPath);
-            TestUtils.DeepCopy(resPath, tmpPath);
-        }
-
-        [TestCleanup]
-        public void TearDown()
-        {
-            TestUtils.DeleteDir(tmpPath);
+            rootPath = TestUtils.AbsPath("Resources/FUnrealService_CreateTest");
         }
 
         class LoggerMock : IFUnrealLogger
@@ -89,11 +76,12 @@ namespace FUnrealTest
         }
 
         [TestMethod]
-        public void CreateForUE_v4_27_2()
+        public void CreateFor_ForeignProject_And_UE_v4_27_2()
         {
-            string uprojectFilePath = TestUtils.PathCombine(tmpPath, "Projects/UE4Prj/UE4Prj.uproject");
-            string engineRootPath = TestUtils.PathCombine(tmpPath, "Engines/UE_4.27/Engine");
-            string vsixDllPath = TestUtils.PathCombine(tmpPath, "faked-funreal.dll");
+            string scenarioPath = TestUtils.PathCombine(rootPath, "Scenario_Foreign");
+            string uprojectFilePath = TestUtils.PathCombine(scenarioPath, "Projects/UE4Prj/UE4Prj.uproject");
+            string engineRootPath = TestUtils.PathCombine(scenarioPath, "Engines/UE_4.27/Engine");
+            string vsixDllPath = TestUtils.PathCombine(rootPath, "faked-funreal.dll"); //faked dll, just used to get is base path and locate templates
 
             var expVersion = new XVersion(4, 27, 2);
             var expUbtPath = TestUtils.PathCombine(engineRootPath, "Binaries/DotNET/UnrealBuildTool.exe");
@@ -116,11 +104,12 @@ namespace FUnrealTest
         }
 
         [TestMethod]
-        public void CreateForUE_v5_1_0()
+        public void CreateFor_ForeignProject_And_UE_v5_1_0()
         {
-            string uprojectFilePath = TestUtils.PathCombine(tmpPath, "Projects/UE5Prj/UE5Prj.uproject");
-            string engineRootPath = TestUtils.PathCombine(tmpPath, "Engines/UE_5.1/Engine");
-            string vsixDllPath = TestUtils.PathCombine(tmpPath, "faked-funreal.dll");
+            string scenarioPath = TestUtils.PathCombine(rootPath, "Scenario_Foreign");
+            string uprojectFilePath = TestUtils.PathCombine(scenarioPath, "Projects/UE5Prj/UE5Prj.uproject");
+            string engineRootPath = TestUtils.PathCombine(scenarioPath, "Engines/UE_5.1/Engine");
+            string vsixDllPath = TestUtils.PathCombine(rootPath, "faked-funreal.dll");
 
             var expVersion = new XVersion(5, 1, 0);
             var expUbtPath = TestUtils.PathCombine(engineRootPath, "Binaries/DotNET/UnrealBuildTool/UnrealBuildTool.exe");
@@ -128,6 +117,34 @@ namespace FUnrealTest
             options.TemplatesMode = TemplateMode.BuiltIn;
 
             var service = FUnrealService.Create(new FUnrealVsMock(uprojectFilePath, engineRootPath, vsixDllPath, FUnrealUEProjectType.Foreign, options));
+
+            var engine = service.Engine;
+            Assert.AreEqual(expVersion, engine.Version);
+            Assert.AreEqual(expUbtPath, engine.UnrealBuildTool.BinPath);
+
+            var plugins = service.PluginTemplates();
+            Assert.AreEqual(2, service.PluginTemplates().Count);
+            Assert.AreEqual("builtin_plugin_0", plugins[0].Name);
+
+            Assert.AreEqual(1, service.PluginModuleTemplates().Count);
+            Assert.AreEqual(1, service.GameModuleTemplates().Count);
+            Assert.AreEqual(2, service.SourceTemplates().Count);
+        }
+
+        [TestMethod]
+        public void CreateFor_NativeProject_And_UE_v5_1_0()
+        {
+            string scenarioPath = TestUtils.PathCombine(rootPath, "Scenario_Native");
+            string uprojectFilePath = TestUtils.PathCombine(scenarioPath, "UE_5.1/UE5Prj/UE5Prj.uproject");
+            string engineRootPath = TestUtils.PathCombine(scenarioPath, "UE_5.1/Engine");
+            string vsixDllPath = TestUtils.PathCombine(rootPath, "faked-funreal.dll");
+
+            var expVersion = new XVersion(5, 1, 0);
+            var expUbtPath = TestUtils.PathCombine(scenarioPath, "UE_5.1/GenerateProjectFiles.bat");
+            var options = new FUnrealTemplateOptionsPage();
+            options.TemplatesMode = TemplateMode.BuiltIn;
+
+            var service = FUnrealService.Create(new FUnrealVsMock(uprojectFilePath, engineRootPath, vsixDllPath, FUnrealUEProjectType.Native, options));
 
             var engine = service.Engine;
             Assert.AreEqual(expVersion, engine.Version);
